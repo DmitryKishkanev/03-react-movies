6f804ab1451de5a67b6b3eed2e242b8b62c00868
Dmitry Kishkanev <150253586+DmitryKishkanev@users.noreply.github.com>
2025-08-08 17:48:14 +0300

    создал css модуль для app

--- src/types/movie.ts
+++ src/types/movie.ts
@@ -1,7 +1,9 @@
-export interface Votes {
-  good: number;
-  neutral: number;
-  bad: number;
+export interface Movie {
+  id: number;
+  poster_path: string;
+  backdrop_path: string;
+  title: string;
+  overview: string;
+  release_date: string;
+  vote_average: number;
 }
-
-export type VoteType = 'good' | 'neutral' | 'bad';
