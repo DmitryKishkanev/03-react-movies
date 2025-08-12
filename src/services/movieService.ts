f48bee1e8aac29c4c7cdb3125b50cd7157cc47df
Dmitry Kishkanev <150253586+DmitryKishkanev@users.noreply.github.com>
2025-08-12 15:41:51 +0300

    изменил movieService

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,1 +1,18 @@
-export default function fetchMovies() {}
+import { useEffect } from 'react';
+import axios from 'axios';
+import type { Movie } from 'types/movie';
+
+const BASE_URL = 'https://api.themoviedb.org/3/search/movie';
+
+export default function fetchMovies({ movie }: Movie) {
+  useEffect(() => {
+    async function fatchMovie() {
+      const response = await axios.get(BASE_URL, {
+        params: {},
+        headers: {
+          Authorization: `Bearer ${VITE_TMDB_TOKEN}`,
+        },
+      });
+    }
+  });
+}
